cfbf104b137e61e0419ee6346763f2d6c8f43765
GrimGrams <GrimGrams@users.noreply.github.com>
2025-09-11 15:25:03 -0400

    added PATH_MAX for string holding file names

--- gflash.h
+++ gflash.h
@@ -1,14 +1,16 @@
 #ifndef GFLASH_H
 #define GFLASH_H
 
+#include <limits.h>
+
 typedef struct{
     char processor[25];
     char port[35];
-    char hex[15];
-    char asmf[15];
-    char copy[25];
-    char inc[100];
-    char wild[100]; // sometimes paths can be so fucking long so ig 100 is fine, right?
+    char hex[PATH_MAX];
+    char asmf[PATH_MAX];
+    char copy[PATH_MAX];
+    char inc[PATH_MAX];
+    char wild[PATH_MAX];
 } OptionStrings;
 
 #endif //GFLASH_H
