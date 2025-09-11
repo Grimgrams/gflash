c9c053139046e90fefc5609fcb4e249e5c6e042f
Julian <julianjpit@gmail.com>
2025-09-11 15:07:52 -0400

    made inc larger (100)

--- gflash.h
+++ gflash.h
@@ -7,7 +7,7 @@ typedef struct{
     char hex[15];
     char asmf[15];
     char copy[25];
-    char inc[25];
+    char inc[100];
     char wild[100]; // sometimes paths can be so fucking long so ig 100 is fine, right?
 } OptionStrings;
 
